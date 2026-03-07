323c6e8c2150186b1fb15dd5ae5636e238399cbf
jao-victor1 <joaovictordemoura664@gmail.com>
2026-03-07 20:04:15 -0300

    Add HTML file for multiplication table application with basic styling and functionality

---
+++ Aa.java
@@ -0,0 +1,7 @@
+public class Aa{
+    public static void main(String[] args) {
+        double a = 3000000000004353453453450000000000.9043333333333333333333333333333333;
+        System.out.println(a);
+        
+    }
+}
