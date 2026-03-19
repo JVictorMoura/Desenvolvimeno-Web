2f75cf3c10521f4c5fd8d5791fe4f96f042f095b
JVictorMoura <joaovictordemoura664@gmail.com>
2026-03-19 12:23:13 -0300

    Add event listener to button and update HTML structure

--- aula-19-03-2026/script.js
+++ aula-19-03-2026/script.js
@@ -2,6 +2,8 @@ let botao1 = document.getElementById("botao1");
 let botao2 = document.getElementById("botao2");
 let campo = document.getElementById("campo");
 let resultado = document.getElementById("resultado");
+let text1 = document.getElementById("text1");
+let button3 = document.getElementById("button3");
 
 botao1.onclick = function () {
     console.log("voce é gay");
@@ -16,15 +18,18 @@ botao2.onmouseover = function () {
     botao2.style.height = "500px";
     botao2.style.fontSize = "40px";
 };
-botao2.onmouseout = function () {
+botao2.onmouseout = function () {// == () =>
     botao2.style = "";//voltar ao padrao
 };
-botao1.ondblclick = function(){
+botao1.ondblclick = () => {
     botao1.style.backgroundColor = "red";
 };
-campo.onkeydown = function(event) {//function 
+campo.onkeydown = (event) =>{//function 
     if(event.key === "Enter"){
         resultado.textContent = campo.value;
         campo.value = "";//zera o campo de entrada
     }
 };
+button3.addEventListener("click", () => {
+    text1.textContent="hajkhsafdjhksfdjkhlfd";
+})
